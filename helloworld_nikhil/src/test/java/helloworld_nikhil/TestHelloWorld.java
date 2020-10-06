d9acec0ba895d24da76729a52794f6b897bd457b
nik <nik@nikhils-mac>
2020-10-06 18:54:52 -0400

    Maven Test Project setup

---
+++ helloworld_nikhil/src/test/java/helloworld_nikhil/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld_nikhil;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println("--Hello World--!");
+	}
+
+}
